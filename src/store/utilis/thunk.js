f54e13903e83ee6e6455f7538c725126e1bd57a8
teosvision <mateovelo99@gmail.com>
2023-04-01 11:54:43 +0200

    mste

--- src/store/utilis/thunk.js
+++ src/store/utilis/thunk.js
@@ -4,7 +4,10 @@ import {
   fetchDataSuccess,
   fetchDataFailure,
 } from "../reducers/images";
-const clientId = "54d381fa15eed99";
+
+const clientId = "f4d5cb8a04710d0";
+const headers = new Headers();
+var data = new FormData();
 //client id : 54d381fa15eed99
 //client secret : fd63012bc9c48bdfbf0d1f846026367a6faa7065
 ////https://api.imgur.com/3/gallery/{{section}}/{{sort}}/{{window}}/{{page}}?showViral={{showViral}}&mature={{showMature}}&album_previews={{albumPreviews}}
@@ -14,21 +17,32 @@ export const fetchImgurData =
     try {
       dispatch(fetchDataStart());
 
-      const response = await axios
-        .get(
-          `https://api.imgur.com/3/gallery/${section}/${sort}/${window}/${page}?showViral=${showViral}&mature=true&album_previews=true`,
-          {
-            headers: {
-              Authorization: `Client-ID ${"54d381fa15eed99"}`,
-            },
-          }
-        )
-        .then((response) => {
-          console.log(response.data);
-        });
+      const response = await axios.get(
+        // `https://api.imgur.com/3/gallery/hot/viral/day/1?showViral=true&mature=true&album_previews=true`
+        `https://api.imgur.com/3/gallery/${section}/${sort}/${window}/${page}?showViral=${showViral}&mature=${showMature}&album_previews=${albumPreviews}`,
+        {
+          headers: {
+            Authorization: `Client-ID ${clientId}`,
+          },
+        }
+      );
+
       console.log(response.data);
       dispatch(fetchDataSuccess(response.data));
     } catch (error) {
       dispatch(fetchDataFailure(error.message));
     }
   };
+
+export const testAxios = async () => {
+  const response = await axios.get(
+    "https://api.imgur.com/3/gallery/hot/viral/day",
+    {
+      headers: {
+        // Authorization: "Client f4d5cb8a04710d0",
+        Authorization: "Client f4d5cb8a04710d0",
+      },
+    }
+  );
+  console.log(response, "RESPONSE");
+};
