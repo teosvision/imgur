1c6adb008d23d538cf4dd338e9481c60a80c1d47
teosvision <mateovelo99@gmail.com>
2023-04-03 17:07:30 +0200

    little changes

--- src/store/utilis/thunk.js
+++ src/store/utilis/thunk.js
@@ -8,11 +8,10 @@ import {
 const clientId = "f4d5cb8a04710d0";
 
 export const fetchImgurData =
-  (section, sort, window, showViral, showMature, albumPreviews, id) =>
+  (section, sort, window, showViral, showMature, albumPreviews) =>
   async (dispatch) => {
     try {
       dispatch(fetchDataStart());
-
       const response = await axios.get(
         `https://api.imgur.com/3/gallery/${section}/${sort}/${window}/1?showViral=${showViral}&mature=${showMature}&album_previews=${albumPreviews}`,
         {
@@ -21,7 +20,6 @@ export const fetchImgurData =
           },
         }
       );
-
       dispatch(
         fetchDataSuccess(response.data.data.filter((post) => post.images))
       );
